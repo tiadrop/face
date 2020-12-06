f950496b0336826d3f3e0c2eb91eaafd4cbdcc2a
thecynicslantern <thecynicslantern@gmail.com>
2020-12-06 04:23:14 +0000

    jel update

--- Face/js-src/jel/index.js
+++ Face/js-src/jel/index.js
@@ -57,6 +57,7 @@ const labeledEdit = (spec, define, trigger) => {
 			$.input({
 				attribs: { id },
 				id: "input",
+				attribs: { name: spec.name },
 				events: {
 					change: ev => trigger("change", ev)
 				}
@@ -77,26 +78,28 @@ const labeledEdit = (spec, define, trigger) => {
 const jel = require("jel").createParser({ labeledEdit })
 
 body.append(jel.labeledEdit({
-	caption: "Favourite pasty"
+	caption: "Favourite pasty",
+	name: "pasty
 }));
 
 
-*/
+*/ "use strict";
 
-const elementWrapperSym = Symbol();
-const Jel = function Jel(type, spec = {}){
-	spec = {...spec};
-	if(spec.classes){
+const elementWrapperCache = new WeakMap();
+
+const Jel = function Jel(type, spec = {}) {
+	spec = { ...spec };
+	if (spec.classes) {
 		spec.classes = Array.isArray(spec.classes) ? spec.classes : spec.classes.split(/\s+/);
 	} else spec.classes = [];
 
-	if(type === undefined) throw new Error("Entity type is undefined");
+	if (type === undefined) throw new Error("Entity type is undefined");
 	const eventHandlers = {};
 	const triggerEvent = (name, data) => {
-		if(eventHandlers[name]) eventHandlers[name].forEach(h => h(data));
+		if (eventHandlers[name]) eventHandlers[name].forEach(h => h(data));
 	};
 
-	// type() (the initialiser) will return this component's root HTMLElement (or 'jelement' that represents it)
+	// type() (the jel-constructor) will return this component's root HTMLElement (or Jel that represents it)
 	const define = props => Object.defineProperties(this, props);
 	define.values = vals => {
 		Object.keys(vals).forEach(k => Object.defineProperty(this, k, { value: vals[k] }));
@@ -108,7 +111,7 @@ const Jel = function Jel(type, spec = {}){
 		Object.keys(vals).forEach(k => Object.defineProperty(this, k, { set: vals[k] }));
 	};
 	define.importDom = (ent) => {
-		["absoluteTop", "absoluteLeft", "style", "attribs", "data", "parent", "qsa"].forEach(k => Object.defineProperty(this, k, {
+		["absoluteTop", "absoluteLeft", "style", "attribs", "data", "qsa"].forEach(k => Object.defineProperty(this, k, {
 			get: () => {
 				return ent[k];
 			}
@@ -121,32 +124,32 @@ const Jel = function Jel(type, spec = {}){
 	this.entityType = type.name;
 
 	let domElement;
-	if(domRoot instanceof Jel){
+	if (domRoot instanceof Jel) {
 		domElement = domRoot.domElement;
-	} else if(domRoot instanceof HTMLElement){
+	} else if (domRoot instanceof HTMLElement) {
 		domElement = domRoot;
-		domRoot[elementWrapperSym] = this;
+		elementWrapperCache.set(domRoot, this);
 	} else throw new Error("Invalid return type from entity definition");
 
-	if(type !== initHtmlNode){
+	if (type !== initHtmlNode) {
 		const rootJel = (domRoot instanceof Jel) ? domRoot : wrapHtmlNode(domRoot);
 		rootJel.entity = this;
 	}
 
-	if(!this.on) this.on = (eventName, handler) => {
-		if(eventHandlers[eventName] === undefined) eventHandlers[eventName] = [];
+	if (!this.on) this.on = (eventName, handler) => {
+		if (eventHandlers[eventName] === undefined) eventHandlers[eventName] = [];
 		eventHandlers[eventName].push(handler);
 	};
-	if(!this.off) this.off = (eventName, handler) => {
-		if(eventHandlers[eventName] === undefined) return;
+	if (!this.off) this.off = (eventName, handler) => {
+		if (eventHandlers[eventName] === undefined) return;
 		const idx = eventHandlers[eventName].indexOf(handler);
-		if (idx !== -1) eventHandlers[eventName].splice(idx);
+		if (idx !== -1) eventHandlers[eventName].splice(idx)
 	};
 
 
-	if(spec.events){
+	if (spec.events) {
 		Object.keys(spec.events).forEach(k => {
-			if(spec.events[k]) this.on(k, spec.events[k]);
+			if (spec.events[k]) this.on(k, spec.events[k]);
 		})
 	}
 
@@ -154,39 +157,41 @@ const Jel = function Jel(type, spec = {}){
 	Object.defineProperties(this, {
 		id: { value: id },
 		domElement: { value: domElement, enumerable: true, },
-		hasAncestor: { value: elOrEnt => {
-			let el = elOrEnt instanceof HTMLElement ? el : elOrEnt.domElement;
-			let p = this.parent;
-			while(p){
-				if(p.domElement === el) return true;
-				p = p.parent;
+		hasAncestor: {
+			value: elOrEnt => {
+				let el = elOrEnt instanceof HTMLElement ? el : elOrEnt.domElement;
+				let p = this.parent;
+				while (p) {
+					if (p.domElement === el) return true;
+					p = p.parent;
+				}
+				return false;
 			}
-			return false;
-		}}
+		}
 	});
 };
 
 const wrapHtmlNode = node => {
-	if(typeof node == "string") node = document.querySelector(node);
-	if(!(node instanceof HTMLElement)) throw new Error("Expecting HTMLElement");
-	if(!node[elementWrapperSym]){
-		// wrap(someNode) == wrap(someNode) should be true
-		node[elementWrapperSym] = new Jel(initHtmlNode, {
+	if (typeof node == "string") node = document.querySelector(node);
+	if (!(node instanceof HTMLElement)) throw new Error("Expecting HTMLElement");
+	if (!elementWrapperCache.has(node)) {
+		// wrap(someNode) == wrap(someNode) should be true so we cache (weakly)
+		elementWrapperCache.set(node, new Jel(initHtmlNode, {
 			id: node.id,
 			wrapping: node,
-		});
+		}));
 	}
-	return node[elementWrapperSym];
+	return elementWrapperCache.get(node);
 };
 
 const parseHtml = html => {
 	const result = [];
 	const tempElement = document.createElement("div");
 	tempElement.innerHTML = html;
-	while(tempElement.childNodes.length){
+	while (tempElement.childNodes.length) {
 		let node = tempElement.childNodes[0];
 		tempElement.removeChild(node);
-		if(!(node instanceof Text)) node = wrapHtmlNode(node);
+		if (!(node instanceof Text)) node = wrapHtmlNode(node);
 		result.push(node);
 	}
 	return result;
@@ -204,13 +209,11 @@ const attributesAccessorHandler = {
 		return true;
 	},
 	ownKeys: (o) => Array.from(o.attributes).map(a => a.name),
-	enumerable: true,	
+	enumerable: true,
 };
+
 const dataAttributesAccessorHandler = {
-	get: (o, k) => {
-		const v = o.getAttribute("data-" + k);
-		return v === null ? undefined : v;
-	},
+	get: (o, k) => o.getAttribute("data-" + k),
 	set: (o, k, v) => {
 		o.setAttribute("data-" + k, v);
 		return true;
@@ -218,7 +221,7 @@ const dataAttributesAccessorHandler = {
 	has: (o, k) => o.hasAttribute("data-" + k),
 	deleteProperty: (o, k) => o.removeAttribute("data-" + k),
 	ownKeys: (o) => Reflect.ownKeys(o.dataset),
-	enumerable: true,	
+	enumerable: true,
 };
 
 const combineSubStyles = source => {
@@ -228,39 +231,24 @@ const combineSubStyles = source => {
 const styleAccessorHandler = {
 	set: (o, k, v) => {
 		let match;
-		if(match = k.match(/^filter_(\w+)$/)){
+		if (match = k.match(/^filter_(\w+)$/)) {
 			o.filters[match[1]] = v;
 			o.element.style.filter = combineSubStyles(o.filters);
 			return true;
 		}
-		if(match = k.match(/^transform_(\w+)$/)){
+		if (match = k.match(/^transform_(\w+)$/)) {
 			o.transforms[match[1]] = v;
 			o.element.style.transform = combineSubStyles(o.transforms);
 			return true;
 		}
 		o.element.style[k] = v;
 		return true;
-	}, 
+	},
 	get: (o, k) => {
-		if(k === "animate" && Jel.twixt){
-			return (duration, toStyles, ease) => {
-				const properties = {};
-				Object.keys(toStyles).forEach(k => {
-					let currentValue;
-					if(match = k.match(/^filter_(\w+)$/)){
-						currentValue = o.filters[match[1]];
-					} else if(match = k.match(/^transform_(\w+)$/)){
-						currentValue = o.transforms[match[1]];
-					} else currentValue = o.element.style[k];	
-					properties.push({ from: currentValue, to: toStyles[k], ease })
-				});
-				return Jel.twixt.animate(o.element.style, duration, properties);
-			};
-		}
-		if(match = k.match(/^filter_(\w+)$/)){
+		if (match = k.match(/^filter_(\w+)$/)) {
 			return o.filters[match[1]];
 		}
-		if(match = k.match(/^transform_(\w+)$/)){
+		if (match = k.match(/^transform_(\w+)$/)) {
 			return o.transforms[match[1]];
 		}
 		return o.element.style[k];
@@ -270,8 +258,7 @@ const styleAccessorHandler = {
 const wrapStyles = element => {
 	const filters = {};
 	const transforms = {};
-	const wrapper = new Proxy({element, filters, transforms}, styleAccessorHandler);
-	return wrapper;
+	return new Proxy({ element, filters, transforms }, styleAccessorHandler);
 };
 
 // ent constructors define entity props and return the domElement they created
@@ -279,35 +266,34 @@ const initHtmlNode = (spec, defineProperties) => {
 	spec = { ...spec };
 	const element = spec.wrapping ? spec.wrapping : document.createElement(spec.tag);
 
-	if(spec.wrapping){
+	if (spec.wrapping) {
 		spec.content = Array.from(element.childNodes);
-	} else { // spec.classes is now always an array (converted earlier)
+	} else { // spec.classes is always an array here
 		let classes = (spec.classes && spec.classes.filter(c => c)) || [];
-		if(classes.length) {
+		if (classes.length) {
 			element.className = classes.join(" ");
 		}
 	}
 
-	if(spec.html){
-		if(spec.content) throw new Error("HTML entity spec can include content OR html");
+	if (spec.html) {
+		if (spec.content) throw new Error("HTML entity spec can include 'content' OR 'html'");
 		spec.content = parseHtml(spec.html);
-		// todo: these jel wrappers are unreferenced; shouldn't we just set element.innerHTML instead?
-		// ^^ probably not; elements with id ARE referenced @ jelement["$id"]
 	}
 
 	const attributesAccessor = new Proxy(element, attributesAccessorHandler);
 	const dataAttributesAccessor = new Proxy(element, dataAttributesAccessorHandler);
 	const styleAccessor = wrapStyles(element);
 
-	if(spec.attribs) Object.keys(spec.attribs).forEach(k => element.setAttribute(k, spec.attribs[k]));
-	if(spec.data) Object.keys(spec.data).forEach(k => element.setAttribute("data-" + k, spec.data[k]));
+	if (spec.attribs) Object.keys(spec.attribs).forEach(k => element.setAttribute(k, spec.attribs[k]));
+	if (spec.data) Object.keys(spec.data).forEach(k => element.setAttribute("data-" + k, spec.data[k]));
 
 	const getAbsoluteLeft = () => {
-		return element.offsetLeft + (element.offsetParent ? wrapHtmlNode(element.offsetParent).absoluteLeft : 0);
+		return element.getBoundingClientRect().left;
 	};
 	const getAbsoluteTop = () => {
-		return element.offsetTop + (element.offsetParent ? wrapHtmlNode(element.offsetParent).absoluteTop : 0);
+		return element.getBoundingClientRect().top;
 	};
+
 
 	const propertyDefs = {
 		enabled: {
@@ -316,62 +302,67 @@ const initHtmlNode = (spec, defineProperties) => {
 		}
 	};
 
-	// pass-through properties
-	["value", "checked"].forEach(prop => {
+	// direct pass-through properties
+	["value", "checked", "autoplay", "spellcheck", "translate", "autofocus", "contentEditable", "lang", "scrollWidth", "scrollHeight", "scrollTop", "scrollLeft", "tabIndex"].forEach(prop => {
 		propertyDefs[prop] = {
 			get: () => element[prop],
 			set: v => element[prop] = v,
 			enumerable: true
 		}
 	});
-	// bound props
-	["click", "getContext", "focus", "blur"].forEach(prop => {
+	// bound methods
+	["click", "getContext", "focus", "blur", "play", "pause", "getContext", "requestFullscreen", "requestPointerLock", "getBoundingClientRect"].forEach(prop => {
 		propertyDefs[prop] = {
 			get: () => element[prop].bind(element)
 		}
 	});
 
-	const addContent = content => {
-		if(!content) return;
+
+	const addContent = (content, prepend = false) => {
+		if (!content) return;
 		let item;
-		if(Array.isArray(content)){
-			content.forEach(addContent);
+		if (Array.isArray(content)) {
+			if (prepend) {
+				content = content.reverse();
+			}
+			content.forEach(c => addContent(c, prepend));
 			return;
 		}
-		if(content instanceof HTMLElement){
+		if (content instanceof HTMLElement) {
 			item = wrapHtmlNode(content);
-		} else if(content instanceof Text){
+		} else if (content instanceof Text || content instanceof Jel) {
 			item = content;
-		} else if(["string", "number"].includes(typeof content)){
+		} else if (["string", "number"].includes(typeof content)) {
 			item = new Text(content);
-		} else if(content instanceof Jel){
-			item = content;
-		} else if(content.type) {
+		} else if (content.type) {
 			item = new Jel(content.type, content);
-		} else throw new Error("Invalid entity type");
+		} else throw new Error("Invalid content type");
 
-		if(item instanceof Text){
-			element.appendChild(item);
+		const append = (!prepend || element.children.length == 0) ? l => element.appendChild(l) : l => element.insertBefore(l, element.children[0]);
+
+		if (item instanceof Text) {
+			append(item);
 			return;
 		}
 
-		if(item.id){
-			propertyDefs["$" + item.id] = {
-				get: () => item, enumerable: true,
-				configurable: true,
-			};	
+		if (item.id) {
+			defineProperties({
+				["$" + item.id]: {
+					get: () => item, enumerable: true,
+					configurable: true,
+				}
+			});
 		}
 
-		defineProperties(propertyDefs);
-		element.appendChild(item.domElement);
+		append(item.domElement);
 	};
 
 	const eventHandlerWrappers = new WeakMap();
 	const wrapEventHandler = fn => {
-		if(!eventHandlerWrappers.has(fn)){
+		if (!eventHandlerWrappers.has(fn)) {
 			eventHandlerWrappers.set(fn, ev => {
-				if(ev.clientX !== undefined && ev.innerX === undefined) ev.innerX = ev.clientX - getAbsoluteLeft();
-				if(ev.clientY !== undefined && ev.innerY === undefined) ev.innerY = ev.clientY - getAbsoluteTop();
+				if (ev.clientX !== undefined && ev.innerX === undefined) ev.innerX = ev.clientX - getAbsoluteLeft();
+				if (ev.clientY !== undefined && ev.innerY === undefined) ev.innerY = ev.clientY - getAbsoluteTop();
 				fn(ev);
 			});
 		}
@@ -386,14 +377,16 @@ const initHtmlNode = (spec, defineProperties) => {
 		off: { value: (eventName, handler) => element.removeEventListener(eventName, wrapEventHandler(handler)) },
 		absoluteLeft: { get: getAbsoluteLeft },
 		absoluteTop: { get: getAbsoluteTop },
+		clientWidth: { get: () => element.clientWidth },
+		clientHeight: { get: () => element.clientHeight },
+
 		qsa: { value: query => Array.from(element.querySelectorAll(query)).map(wrapHtmlNode) },
 		parent: { get: () => element.parentElement && wrapHtmlNode(element.parentElement), },
-		style: { 
+		style: {
 			get: () => styleAccessor,
 			set: (k, v) => { element.style[k] = v; },
-			enumerable: true 
+			enumerable: true
 		},
-		tag: { get: () => element.tagName.toLowerCase() },
 		content: {
 			get: () => [...element.childNodes].map(node => node instanceof Text ? node : wrapHtmlNode(node)),
 			set: v => {
@@ -416,24 +409,26 @@ const initHtmlNode = (spec, defineProperties) => {
 			enumerable: true
 		},
 		append: { value: addContent, enumerable: true },
-		remove: { value: entityToRemove => {
-			const elementToRemove = entityToRemove instanceof Jel ? entityToRemove.domElement : entityToRemove;
-			if(!(entityToRemove instanceof Jel)) throw new Error("Invalid type");
-			element.removeChild(elementToRemove);
-			//children.splice(children.indexOf(entityToRemove), 1);
-			if(entityToRemove instanceof Jel && entityToRemove.id){
-				delete entity["$" + entityToRemove.id];
+		prepend: { value: l => addContent(l, true), enumerable: true },
+		remove: {
+			value: entityToRemove => {
+				const elementToRemove = entityToRemove instanceof Jel ? entityToRemove.domElement : entityToRemove;
+				if (!(entityToRemove instanceof Jel)) throw new Error("Invalid type");
+				element.removeChild(elementToRemove);
+				if (entityToRemove instanceof Jel && entityToRemove.id) {
+					delete entity["$" + entityToRemove.id];
+				}
 			}
-		}},
+		},
 		...propertyDefs
 	});
 
 
-	if(spec.content){
+	if (spec.content) {
 		addContent(spec.content);
 	}
 
-	if(spec.style){
+	if (spec.style) {
 		Object.keys(spec.style).forEach(k => styleAccessor[k] = spec.style[k]);
 	}
 
@@ -441,48 +436,36 @@ const initHtmlNode = (spec, defineProperties) => {
 	return element;
 };
 
-const appendToParent = (child, parent) => {
-	if(parent instanceof Jel){
-		parent.append(child);
-	} else if(parent instanceof HTMLElement){
-		parent.appendChild(child.domElement);
-	} else throw new Error("Incompatible type");
+const domFunc = source => {
+	if (source instanceof HTMLElement) return wrapHtmlNode(source);
+	if (typeof source == "string") {
+		if (source[0] === "<") {
+			return parseHtml(source)[0];
+		} else {
+			return wrapHtmlNode(document.querySelector(source));
+		}
+	} else {
+		throw new Error("Invalid argument");
+	}
 };
 
 const createParser = (entityTypes = {}) => {
 	let customTypes = {};
 	Object.keys(entityTypes).forEach(k => {
-		customTypes[k] = (spec, appendTo) => {
-			const entity = new Jel(entityTypes[k], spec);
-			if(appendTo) appendToParent(entity, appendTo);
-			return entity;
+		customTypes[k] = spec => {
+			return new Jel(entityTypes[k], spec);
 		}
 	});
 
-	const domFunc = source => {
-		if(source instanceof HTMLElement) return wrapHtmlNode(source);
-		if(typeof source == "string"){
-			if(source[0] === "<"){
-				return parseHtml(source)[0];
-			} else {
-				return wrapHtmlNode(document.querySelector(source));
-			}
-		} else {
-			throw new Error("Invalid argument");
-		}
-	};
-
 	return {
-		dom: new Proxy(() => {}, {
+		dom: new Proxy(() => { }, {
 			apply: (o, _, args) => domFunc(...args),
 			get: (o, k) => {
 				return (spec = {}) => {
-					const entity = new Jel(initHtmlNode, {...spec, tag: k});
-					return entity;
+					return new Jel(initHtmlNode, {...spec, tag: k});
 				}
 			}
 		}),
-		createType: configurator => (spec = {}) => new Jel(configurator, spec),
 		...customTypes
 	};
 };
